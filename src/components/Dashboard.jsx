22fa7a9985e538f05b8558bff7b9c6952ddde7dc
AungThuraHein <u6726135@au.edu>
2026-01-11 21:06:04 +0700

    Time DropDown

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import { LineChart, PieChart, BarChart } from 'recharts';
 
 
 
 //Note: After pulling the repo, run "npm install" to install all dependencies :D
 function Dashboard() {
+    const [period, setPeriod] = useState('daily');
     return(
-        <p> Dashboard </p>
+        <div>
+        <div>
+        <h2> Dashboard </h2>
+        </div>
+
+        <div>
+            <label>Sales Trend View: </label>
+            <select value={period} onChange={(e) => setPeriod(e.target.value)}>
+                <option value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+            </select>
+        </div>
+        
+
+
+
+
+        </div>
     )
 }
 
