d92b1f69eccc3fd6eb8136f68455658166f90fda
AungThuraHein <u6726135@au.edu>
2026-01-11 15:00:06 +0700

    PageLoadings w Button, ReadMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import Dashboard from './components/Dashboard'
+import SalesJournal from './components/SalesJournal'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[currentPage, setCurrentPage] = useState('Dashboard');
 
   return (
-    //
+    <div className='app'>
+      <header className = 'header'>
+       <h1>Basic Point of Sale System</h1>
+       <nav>
+          <button onClick={() => setCurrentPage('Dashboard')}>Dashboard</button>
+          <button onClick={() => setCurrentPage('SalesJounral')}>Sales Journal</button>
+       </nav>
+      </header>
+    </div>
   )
 }
 
